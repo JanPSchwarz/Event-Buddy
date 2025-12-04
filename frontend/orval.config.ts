579eb051987c1ced01d6364bab48aa44d6d325c6
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-12-04 12:35:38 +0100

    orval config added

---
+++ frontend/orval.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "orval";
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const baseURL = isDev
+    ? "http://localhost:8080"
+    : "https://teamproject-wrms.onrender.com/";
+export default defineConfig( {
+    eventBuddyApi: {
+        input: {
+            target: "./openapi.json",
+        },
+        output: {
+            mode: "tags-split",
+            target: "src/api/generated",
+            client: "react-query",
+            baseUrl: baseURL,
+        },
+    },
+} );
