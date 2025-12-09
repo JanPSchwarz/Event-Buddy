a4e850a5c31db12d67d0eb50ab328b661b17e15a
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-12-09 11:14:14 +0100

    Add 404 page not found component and related assets

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import ErrorPage from "@/pages/ErrorPage";
 import { createBrowserRouter } from "react-router";
 import RootLayout from "@/layouts/RootLayout.tsx";
+import PageNotFound from "@/pages/PageNotFound.tsx";
 
 export const routes = createBrowserRouter( [
     {
@@ -9,5 +10,8 @@ export const routes = createBrowserRouter( [
         errorElement: <ErrorPage/>,
         children: [
             // Define child routes
+            { path: "/profile", element: <div>Profile Page</div> },
+            { path: "/settings", element: <div>Settings Page</div> },
+            { path: "*", element: <PageNotFound/> }
         ],
     } ] );
