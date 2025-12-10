82996912fb6f482eb63262572c978ee936a8fc20
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-12-10 17:50:47 +0100

    Refactor authentication logic by introducing AuthService and updating AuthController to improve user management and authentication flow

--- backend/src/main/java/org/eventbuddy/backend/controller/AuthController.java
+++ backend/src/main/java/org/eventbuddy/backend/controller/AuthController.java
@@ -6,29 +6,30 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.eventbuddy.backend.exceptions.UnauthorizedException;
 import org.eventbuddy.backend.models.app_user.AppUser;
 import org.eventbuddy.backend.models.error.ErrorMessage;
-import org.eventbuddy.backend.services.UserService;
+import org.eventbuddy.backend.services.AuthService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 @Tag(name = "Authentication", description = "Get current authenticated user information")
 @Validated
 public class AuthController {
 
-    UserService userService;
+    AuthService authService;
 
-    public AuthController( UserService userService ) {
-        this.userService = userService;
+    public AuthController( AuthService authService ) {
+        this.authService = authService;
     }
 
 
@@ -53,17 +54,13 @@ public class AuthController {
                     schema = @Schema(implementation = ErrorMessage.class)
             )
     )
-    public ResponseEntity<AppUser> getMe( OAuth2AuthenticationToken authentication ) {
+    public ResponseEntity<AppUser> getMe( OAuth2AuthenticationToken authToken ) {
 
-        if ( authentication == null || !authentication.isAuthenticated() ) {
+        if ( authService.isNotAuthenticated( authToken ) ) {
             throw new UnauthorizedException( "You are not logged in." );
         }
 
-        OAuth2User user = authentication.getPrincipal();
-        String provider = authentication.getAuthorizedClientRegistrationId();
-        String providerId = provider + "_" + user.getName();
-
-        AppUser appUser = userService.getAppUserByProviderId( providerId );
+        AppUser appUser = authService.getAppUserByAuthToken( authToken );
 
         return ResponseEntity.ok( appUser );
     }
---
+++ backend/src/main/java/org/eventbuddy/backend/services/AuthService.java
@@ -0,0 +1,105 @@
+package org.eventbuddy.backend.services;
+
+import lombok.RequiredArgsConstructor;
+import org.eventbuddy.backend.configs.AdminConfig;
+import org.eventbuddy.backend.enums.Role;
+import org.eventbuddy.backend.exceptions.UnauthorizedException;
+import org.eventbuddy.backend.models.app_user.AppUser;
+import org.eventbuddy.backend.models.app_user.UserSettings;
+import org.eventbuddy.backend.repos.UserRepository;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService extends DefaultOAuth2UserService {
+    private final UserRepository userRepo;
+    private final AdminConfig adminConfig;
+
+
+    @Override
+    public OAuth2User loadUser( OAuth2UserRequest userRequest ) {
+        OAuth2User oAuthUser = super.loadUser( userRequest );
+
+
+        String uniqueProviderId = getProviderIdByRequestAndUser( userRequest, oAuthUser );
+
+        System.out.println( "is admin: " + adminConfig.isAdmin( uniqueProviderId ) );
+
+        // create user if not exists
+        AppUser user = userRepo.findByProviderId( uniqueProviderId ).orElseGet( () ->
+                createAndSaveUser( oAuthUser, userRequest )
+        );
+
+        return oAuthUser;
+    }
+
+    public boolean userExistsById( String userId ) {
+        return userRepo.existsById( userId );
+    }
+
+    public AppUser getAppUserByAuthToken( OAuth2AuthenticationToken authToken ) {
+        String providerId = getProviderIdByAuthToken( authToken );
+        return getAppUserByProviderId( providerId );
+    }
+
+    public boolean isNotAuthenticated( OAuth2AuthenticationToken authToken ) {
+        return authToken == null || !authToken.isAuthenticated();
+    }
+
+
+    public boolean isRequestUserOrSuperAdmin( OAuth2AuthenticationToken authToken, String userId ) {
+        if ( isNotAuthenticated( authToken ) ) {
+            throw new UnauthorizedException( "You are not logged in." );
+        }
+
+        AppUser user = getAppUserByAuthToken( authToken );
+
+        return user.getId().equals( userId ) || user.getRole() == Role.SUPER_ADMIN;
+    }
+
+    private AppUser getAppUserByProviderId( String providerId ) {
+        return userRepo.findByProviderId( providerId )
+                .orElseThrow( () -> new RuntimeException( "User not found with providerId: " + providerId ) );
+    }
+
+    private AppUser createAndSaveUser( OAuth2User oAuthUser, OAuth2UserRequest userRequest ) {
+
+        String uniqueProviderId = getProviderIdByRequestAndUser( userRequest, oAuthUser );
+
+        UserSettings settings = UserSettings.builder()
+                .userVisible( true )
+                .showOrgas( true )
+                .showAvatar( true )
+                .build();
+
+        AppUser newUser = AppUser.builder()
+                .providerId( uniqueProviderId )
+                .name( oAuthUser.getAttribute( "login" ) )
+                .role( adminConfig.isAdmin( uniqueProviderId ) ? Role.SUPER_ADMIN : Role.USER )
+                .email( oAuthUser.getAttribute( "email" ) )
+                .avatarUrl( oAuthUser.getAttribute( "avatar_url" ) )
+                .userSettings( settings )
+                .build();
+
+        return userRepo.save( newUser );
+    }
+
+    private String getProviderIdByRequestAndUser( OAuth2UserRequest userRequest, OAuth2User oAuthUser ) {
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String providerId = oAuthUser.getName();
+        return provider + "_" + providerId;
+    }
+
+    private String getProviderIdByAuthToken( OAuth2AuthenticationToken authToken ) {
+        OAuth2User user = authToken.getPrincipal();
+        String providerId = user.getName();
+        String provider = authToken.getAuthorizedClientRegistrationId();
+        return provider + "_" + providerId;
+    }
+
+
+}
